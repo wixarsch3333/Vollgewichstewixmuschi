368a4beec5ed9e30493cbd414937b85504b6f2ad
wixarsch3333 <sany13.mc.a@gmail.com>
2024-09-04 17:47:17 +0200

    Add files via upload

---
+++ fivv clean.bat
@@ -0,0 +1,60 @@
+@echo off
+
+echo Current user privileges: %userprofile%
+echo.
+echo Requesting administrative privileges...
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :continue
+) else (
+    goto :admin
+)
+
+:admin
+echo.
+echo You need to run this batch file as an Administrator.
+echo Please grant administrative privileges by selecting "Yes" when prompted.
+echo.
+powershell -Command "Start-Process '%0' -Verb RunAs"
+exit
+
+:continue
+echo Administrative privileges confirmed.
+echo.
+sc stop vgk
+sc stop vgc
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 3
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverrideMask /t REG_DWORD /d 3
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 0x000000
+bcdedit /set hypervisorlaunchtype off
+reg add HKLM\SYSTEM\CurrentControlSet\Control\CI\Config /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 0x000000
+powershell.exe -ExecutionPolicy Bypass -Command "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+powershell -Command "& {Get-AppxPackage -AllUsers xbox | Remove-AppxPackage}"
+sc stop XblAuthManager
+sc stop XblGameSave
+sc stop XboxNetApiSvc
+sc stop XboxGipSvc
+sc delete XblAuthManager
+sc delete XblGameSave
+sc delete XboxNetApiSvc
+sc delete XboxGipSvc
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /f
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /disable
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTaskLogon" /disableL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
+cls
+set hostspath=%windir%\System32\drivers\etc\hosts
+echo 127.0.0.1 xboxlive.com >> %hostspath%
+echo 127.0.0.1 user.auth.xboxlive.com >> %hostspath%
+echo 127.0.0.1 presence-heartbeat.xboxlive.com >> %hostspath%
+
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography" /v MachineGuid /f
+)
+
+echo.
+echo Please Restart Your PC
+echo.
+pause>nul
+exit
